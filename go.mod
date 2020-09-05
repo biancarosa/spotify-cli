e3f84012fb7e68f47c45b0890c07e5a2db461be6
Bianca Rosa <me@biancarosa.com.br>
2020-09-05 20:58:53 -0300

    load env files from .env

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/biancarosa/spotify-cli
 go 1.15
 
 require (
+	github.com/joho/godotenv v1.3.0
 	github.com/zmb3/spotify v0.0.0-20200814173021-9bec46940cc0
 	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d
 )
--- main.go
+++ main.go
@@ -4,10 +4,17 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/joho/godotenv"
+
 	"github.com/biancarosa/spotify-cli/cli"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println(".env file not found")
+	}
+
 	if len(os.Args) < 2 {
 		fmt.Println("Envie pelo menos um argumento")
 		return
